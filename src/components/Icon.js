a07c36b62ce593b896dbcc0fb8810bf61c5f44fb
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2020-02-02 16:48:38 -0800

    Separated search icon into component. Updated search animaiton.

---
+++ src/components/Icon.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import gsap from 'gsap';
+
+class Icon extends React.Component {
+
+  searchAnimation = () => {
+    const icon = document.getElementById('icon')
+    const clear = document.getElementById('clear-icon')
+    const tl = gsap.timeline()
+
+    tl
+      .to(icon, .1, {className: '-= fas fa-search'})
+      .to(icon, 4, {className: '+= fas fa-spinner spin-animation'})
+      .to(icon, .5, {opacity: 0, visibility: 'hidden'})
+      .to(clear, .5, {opacity: 1, visibility: 'visible'})
+
+  }
+
+  handleClick = e => {
+    this.searchAnimation()
+    this.props.search(e)
+  }
+
+  clearSearch = () => {
+    const results = document.querySelectorAll('.result')
+    const icon = document.getElementById('icon')
+    const clear = document.getElementById('clear-icon')
+    const tl = gsap.timeline()
+
+    tl
+      .to(clear, .1, {opacity: 0, visibility: 'hidden'})
+      .to(icon, .1, {opacity: 1, visibility: 'visible', className: '-= fas fa-search '})
+
+    this.props.clear()
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <i
+          className="fas fa-search" id="icon"
+          onClick={this.handleClick}
+          onMouseOver={this.props.animate}
+          onMouseOut={this.props.reset}
+        />
+        <i
+          className="fas fa-times"
+          id="clear-icon"
+          onClick={this.clearSearch}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+export default Icon
