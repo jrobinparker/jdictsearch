d2c0389258e3f92e509207e0145299fa931bb889
Jeremy Parker <jrobinparker@gmail.com>
2020-02-02 16:48:38 -0800

    Separated search icon into component. Updated search animaiton.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import gsap from 'gsap';
+import Icon from './Icon.js';
 
 class Search extends React.Component {
 
@@ -15,23 +16,7 @@ class Search extends React.Component {
 
   onFormSubmit = e => {
       e.preventDefault();
-      this.searchAnimation();
       this.props.onTermSubmit(this.state.searchTerm);
-  }
-
-  searchAnimation = () => {
-    const icon = document.getElementById('icon')
-    const clear = document.getElementById('clear-icon')
-    const results = document.querySelectorAll('.result')
-    const searchIconTimeline = gsap.timeline()
-
-    searchIconTimeline
-      .to(icon, .1, {className: '-= fas fa-search '})
-      .to(icon, .1, {className: '+= fas fa-spinner'})
-      .to(icon, {duration: 1, rotate: 360, repeat: 3})
-      .to(icon, .5, {opacity: 0, visibility: 'hidden'})
-      .to(clear, .5, {opacity: 1, visibility: 'visible'})
-
   }
 
   animateIcon = () => {
@@ -44,34 +29,33 @@ class Search extends React.Component {
     gsap.to(icon, .5, {backgroundColor: '#f6f6f6'})
   }
 
-  clearAll = () => {
+  clearSearch = () => {
     this.setState({
       searchTerm: ''
     })
-    const results = document.querySelectorAll('.result')
-    const icon = document.getElementById('icon')
-    const clear = document.getElementById('clear-icon')
-    const tl = gsap.timeline()
-
-    tl
-      .to(clear, .1, {opacity: 0, visibility: 'hidden'})
-      .to(icon, .1, {opacity: 1, visibility: 'visible', className: '-= fas fa-search '})
-
     this.props.handleReload()
   }
 
   render() {
     return (
           <form onSubmit={this.onFormSubmit}>
-              <input type="text" className="input-text" placeholder="enter an english word..." name="searchTerm" value={this.state.searchTerm} onChange={this.onChange}
-              onFocus={this.animateIcon} onBlur={this.resetIcon}
+              <input
+                type="text"
+                className="input-text"
+                placeholder="enter an english word..."
+                name="searchTerm"
+                value={this.state.searchTerm}
+                onChange={this.onChange}
+                onFocus={this.animateIcon}
+                onBlur={this.resetIcon}
               />
-              <i className="fas fa-search" id="icon"
-              onClick={this.onFormSubmit}
-              onMouseOver={this.animateIcon}
-              onMouseOut={this.resetIcon}
+              <Icon
+                search={this.onFormSubmit}
+                clear={this.clearSearch}
+                animate={this.animateIcon}
+                reset={this.resetIcon}
+                clear={this.clearSearch}
               />
-              <i className="fas fa-times" id="clear-icon" onClick={this.clearAll} />
           </form>
     )
   }
