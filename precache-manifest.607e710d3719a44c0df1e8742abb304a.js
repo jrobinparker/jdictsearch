22334420c7d57f299d23b66abb2e17d52bfac348
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2020-02-06 13:36:34 -0800

    Updates

---
+++ precache-manifest.607e710d3719a44c0df1e8742abb304a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e32e5c9720a366d4479745cb2287477",
+    "url": "/jdictsearch/index.html"
+  },
+  {
+    "revision": "f2a17077ae3eec5c41dd",
+    "url": "/jdictsearch/static/css/main.c0096568.chunk.css"
+  },
+  {
+    "revision": "384be88ebb5401df228a",
+    "url": "/jdictsearch/static/js/2.021ab359.chunk.js"
+  },
+  {
+    "revision": "f2a17077ae3eec5c41dd",
+    "url": "/jdictsearch/static/js/main.d0e2ad64.chunk.js"
+  },
+  {
+    "revision": "d750b42d5bcfa3bcce3f",
+    "url": "/jdictsearch/static/js/runtime~main.16bda6ef.js"
+  }
+]);
