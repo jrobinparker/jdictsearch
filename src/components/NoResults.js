d2aebb990930f7f10cb127a8970e26e6e695905d
Jeremy Parker <jrobinparker@gmail.com>
2020-11-04 13:00:48 -0800

    Converted NoResults component into a functional hooks component

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
 
-class NoResults extends React.Component {
-  
-  componentDidMount() {
+const NoResults = () => {
+
+  useEffect(() => {
       const msg = document.querySelector('.no-results')
       gsap.to(msg, .5, {opacity: 1, y: -5})
-  }
+  }, [])
 
-  render() {
     return (
       <div className="no-results">
         No results found!
       </div>
     )
-  }
 }
 
 export default NoResults
