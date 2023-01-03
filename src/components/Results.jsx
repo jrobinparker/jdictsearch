5bb1e5314e122c614631665ca379cc32af036742
Jeremy Parker <jeremy.parker@telusagriculture.com>
2023-01-03 14:13:37 -0800

    Refactor results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,65 +1,71 @@
 import React, { useState, useEffect } from "react";
 import Result from "./Result";
 
-const Results = ({ weblio, eiNavi, eijiro, term, showResults, loading }) => {
-  const [weblioResult, setWeblioResult] = useState([]);
-  const [eiNaviResult, setInfoSeekResult] = useState([]);
-  const [eijiroResult, setEijiroResult] = useState([]);
+const Results = ({ weblio, eiNavi, eijiro, term, showResults }) => {
+  const [results, setResults] = useState({
+    weblio: [],
+    eiNavi: [],
+    eijiro: []
+  });
 
   useEffect(() => {
-    if (weblio) {
-      const weblioSearch = weblio.split("主な意味").toString().split("、");
-      setWeblioResult(weblioSearch);
+    if (weblio.length) {
+      setResults({
+        ...results,
+        weblio: [...weblio.split("主な意味").toString().split("、")]
+      });
     }
 
-    if (eiNavi) {
-      const eiNaviArray = eiNavi.split("、");
-      setInfoSeekResult(eiNaviArray);
+    if (eiNavi.length) {
+      setResults({
+        ...results,
+        eiNavi: [...eiNavi.split("、")]
+      });
     }
 
-    if (eijiro) {
-      const eijiroSearchFullArray = eijiro.split("<a");
-      const eijiroSplitArray = eijiroSearchFullArray[0].split("【レベル】");
-      const eijiroSplitArray2 = eijiroSplitArray[0].split("、");
-      setEijiroResult(eijiroSplitArray2);
+    if (eijiro.length) {
+      setResults({
+        ...results,
+        eijiro: [...eijiro.split("<a")[0].split("【レベル】")[0].split("、")]
+      });
     }
-  }, [weblio, eiNavi, eijiro]);
+  }, [results.weblio, results.eiNavi, results.eijiro]);
 
   let weblioComponent, eiNaviComponent, eijiroComponent;
 
-  if (weblioResult.length) {
+  if (results.weblio.length) {
     weblioComponent = (
       <Result
         name="weblio"
         url={`https://ejje.weblio.jp/content/${term}`}
-        length={weblioResult.length}
-        text={weblioResult}
+        length={results.weblio.length}
+        text={results.weblio}
         term={term}
         appear={showResults}
       />
     );
   }
 
-  if (eiNaviResult.length) {
+  if (results.eiNavi.length) {
     eiNaviComponent = (
       <Result
         name="ei-navi"
         url={`https://www.ei-navi.jp/dictionary/content/${term}/`}
-        length={eiNaviResult.length}
-        text={eiNaviResult}
+        length={results.eiNavi.length}
+        text={results.eiNavi}
         term={term}
         appear={showResults}
       />
     );
   }
 
-  if (eijiroResult.length) {
+  if (results.eijiro.length) {
     eijiroComponent = (
       <Result
         name="eijiro"
         url={`https://eow.alc.co.jp/search?q=${term}&ref=sa`}
-        length={eijiroResult.length}
-        text={eijiroResult}
+        length={results.eijiro.length}
+        text={results.eijiro}
         term={term}
         appear={showResults}
       />
